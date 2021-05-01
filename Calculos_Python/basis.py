7b294f84da47b939d8f1949392d5cffef0da4291
GastoonMeghi <gmeghinasso@est.frba.utn.edu.ar>
2021-05-01 00:43:55 -0300

    separo funciones en archivo movement_assigment

---
+++ Calculos_Python/basis.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+####################################### Funciones de rotación de puntos ################################################
+def x_rotation_matrix(r_x):
+    return np.array([[1,0,0],[0,np.cos(r_x),np.sin(r_x)],[0,-np.sin(r_x),np.cos(r_x)]]) 
+    
+def y_rotation_matrix(r_y):
+    return np.array([[np.cos(r_y),0,-np.sin(r_y)],[0,1,0],[np.sin(r_y),0,np.cos(r_y)]])
+
+def rotate_in_y_axis(point,r_y):
+    W=y_rotation_matrix(r_y)
+    return np.dot(W,point)
+
+def rotate_in_x_axis(point,r_x):
+    W=y_rotation_matrix(r_x)
+    return np.dot(W,point)
+
+####################################### Funciones de cambio de base ################################################
+def get_m2_basis():
+    #Función que retorna la matriz de cambio de base desde la canónica a la base del motor2
+    # La base se genera de la misma forma que el motor 3
+    Wm2=np.array([[-np.cos(np.pi/3),np.cos(np.pi/6),0],[-np.sin(np.pi/3),-np.sin(np.pi/6),0],[0,0,1]])
+    return Wm2
+
+def get_m3_basis():
+    #Función que retorna la matriz de cambio de base desde la canónica a la base del motor3
+    #El sistema de referencia del motor 3 se define de la siguiente forma:
+    ## El eje y positivo es paralelo al plano x-y canónico y su dirección y sentido está determinada por el vector
+    ## que va desde el eje del motor 3 al centro de la plaforma
+    ## el eje x positivo es paralelo al plano x-y canónico y se encuentra perpendicular al eje 'y' y a la derecha del mismo
+    ## El eje z es el mismo que el de la base canónica
+    Wm3=np.array([[-np.cos(np.pi/3),-np.cos(np.pi/6),0],[np.sin(np.pi/3),-np.sin(np.pi/6),0],[0,0,1]])
+    return Wm3
+def get_m1_basis():
+    #Función que retorna la matriz de cambio de base desde la canónica a la base del motor1
+    # La base del motor 1 corresponde a la base canónica
+    Wm1=np.identity(3)
+
+
+def base_change_cannon_to_m1(point):
+    return point
+
+
+def base_change_cannon_to_m2(point):
+    Wm2=get_m2_basis()
+    return np.linalg.solve(Wm2,point)
+
+def base_change_cannon_to_m3(point):
+    Wm3=get_m3_basis()
+    return np.linalg.solve(Wm3,point)
+
+def base_change_m3_to_cannon(point):
+    Wm3=get_m3_basis()
+    return np.dot(Wm3,point)
+
+def base_change_m2_to_cannon(point):
+    Wm2=get_m2_basis()
+    return np.dot(Wm2,point)
+
+def base_change_m1_to_cannon(point):
+    Wm1=get_m1_basis()
+    return np.dot(Wm1,point)
--- Calculos_Python/movement_assignment.py
+++ Calculos_Python/movement_assignment.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cmath
-
+import basis 
 def assign_movement(r_x,r_y):
     "Segunda versión de la función para asignar el movimiento"
     "toma como parametros los dos ángulos en el eje x y en el eje y"
@@ -8,13 +8,13 @@ def assign_movement(r_x,r_y):
         
     #obtengo el gradiente del plano que pasa por (0,0,0)
     #y cumple con los ángulos r_x y r_y
-    grad_plane=np.array([np.tan(r_x),np.tan(r_y))]) 
+    grad_plane=np.array([np.tan(r_x),np.tan(r_y)]) 
     ball_dir=-grad_plane #La dirección en la cual se desplaza la pelota es la del -grad(plano)
-
+                         #Este es el caso ideal sin las restricciones de la plataforma   
     #Aproximo la dirección deseada a algunas de las posibles dadas las restricciones de la plataforma
     ##Convertimos ball_dir a coordenadas polares y nos quedamos con el ángulo para compararlo con las direcciones
     ##posibles
-    angle=cmath.polar(ball_dir[0]+1j*r_ball_dir[1])[1]*(180/np.pi)
+    angle=cmath.polar(ball_dir[0]+1j*ball_dir[1])[1]*(180/np.pi)
     ##Dividimos los 360 grados en las direcciones posible y asignamos el movimiento
     sentido=1 #uso esta variable para invertir el sentido del vector dado que trabajo con cuadrantes 1 y 2
     if angle<0:
@@ -23,8 +23,7 @@ def assign_movement(r_x,r_y):
     if angle > 165:
         movement= "roll"
         joint="C1"
-        roll_vector=pol2rect(1,180 * np.pi/180) #vector que indica la dirección y sentido en la cual se mueve la pelota
-                                                #es la mejor aproximación del -grad(plano) que permite la plataforma
+        roll_vector=pol2rect(1,180 * np.pi/180) #vector que indica la dirección y sentido en la cual se mueve la pelota                                      #es la mejor aproximación del -grad(plano) que permite la plataforma
     if angle > 135 and angle <= 165:
         movement= "pitch"
         joint="C2"
@@ -52,16 +51,38 @@ def assign_movement(r_x,r_y):
 
     roll_vector=roll_vector*sentido #corrijo el sentido si es necesario
     #Calculo el ángulo que se debe realizar en el movimiento
-    movement_angle=calculate_angle(ball_dir)
-    return joint,movement,movement
+    movement_angle=calculate_angle(ball_dir,roll_vector,joint,movement)
+    return joint,movement,movement_angle
 
 
 def pol2rect(mod,ang):
     #Función que realiza la conversión de polar a cartesiana
-    dir=cmath.exp(1j*ang*cmath.pi/180)
-    return np.array([dir.real,dir.imag])
+    dir=cmath.exp(1j*ang)
+    return np.array([dir.real,dir.imag,0])
 
-def calculate_angle(ball_dir):
+def calculate_angle(ball_dir,roll_vector,joint,movement):
     #Función que obtiene el ángulo de inclinación que se le va a dar a la junta actuada
     #Usa el mismo ángulo que el del grandiente original
-    return np.arctan(np.linalg.norm(ball_dir))
+    angle=np.arctan(np.linalg.norm(ball_dir))
+    ##determino el sentido de movimiento para determinar si el ángulo es positivo o negativo
+    #Cambio el sistema de referencia al de la junta que debe actuarse
+    print(roll_vector)
+    print(joint)
+    print(movement)
+    if joint=="C1":
+        roll_vector=basis.base_change_cannon_to_m1(roll_vector)
+    if joint=="C2":
+        roll_vector=basis.base_change_cannon_to_m2(roll_vector)
+    if joint=="C3":
+        roll_vector=basis.base_change_cannon_to_m3(roll_vector)
+    if movement=="roll" and roll_vector[0]<0:
+        #Si el movimiento es de roll y la dirección del roll en el marco de referencia del motor/junta
+        # indicado es en la dirección de las x's negativas, entonces el ángulo debe ser negativo
+        angle=angle*(-1)
+
+    if movement=="pitch" and roll_vector[1]<0:
+        #Si el movimiento es de pitch y la dirección del pitch en el marco de referencia del motor/junta
+        # indicado es en la dirección de las y's negativas, entonces el ángulo debe ser negativo
+        angle=angle*(-1)
+
+    return angle
